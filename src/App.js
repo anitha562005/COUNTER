f22df4fd333dcd103cb06399ac9292d278cef2aa
Anitha <anithaanbazhagan2005@gmail.com>
2025-07-05 11:01:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+
 import logo from './logo.svg';
 import './App.css';
+import Counter from "./counter";
+import React, { useState } from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [showParagraph, setShowParagraph] = useState(false); 
+  const[name, setName]=useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? "container darkMode" : "container"}>
+      <h1>Click Counter App</h1>
+      <input 
+      type="text"
+      placeholder="enter your name"
+      value={name}
+      onChange={(e)=> setName(e.target.value)}/><p>Hello,{name?name:"User"}</p>
+      <button className="btn" onClick={() => setDarkMode(!darkMode)}>
+        {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+      </button>
+      <button className="btn" onClick={() => setShowParagraph(!showParagraph)}>
+        {showParagraph ? "Hide Paragraph" : "Show Paragraph"}
+      </button>
+        <Counter/>
+      {showParagraph && (
+        <p>The counter keeps track of how many times you click the button.</p>
+      )}
     </div>
   );
 }
---
+++ src/counter.js
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+
+
+function Counter() {
+    const[count,setCount]=useState(0);
+    return (
+        <div>
+            <h2>Count: {count}</h2>
+            <button className='btn' onClick={()=>setCount(count +1)}>Increment</button>
+            <button className='btn' onClick={()=>setCount(count -1)}>Decrement</button>
+            <button className='reset btn' onClick={() =>setCount(0)}>Reset</button>
+        </div>
+        
+    );
+}
+
+export default Counter;
+
